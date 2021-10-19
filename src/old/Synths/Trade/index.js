8b8cb6a645b19641b7d2b0e388c896ca1f70fa8d
coinlordd <35095979+coinlordd@users.noreply.github.com>
2021-10-19 10:20:36 +0200

    remove old code

--- src/old/Synths/Trade/index.js
+++
@@ -1,5 +0,0 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
-
-export { default as InputBoxSimple } from './InputBoxSimple'
-export { default as Slider } from './Slider'
